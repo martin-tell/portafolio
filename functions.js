128908b0f0f9d0ebacf985b5e8972e1cd2e43ca3
martin-tell <martin.tello.lpz@gmail.com>
2023-07-06 14:39:17 -0600

    desplazamiento arreglado

--- functions.js
+++ functions.js
@@ -10,3 +10,27 @@ function typeWriter(element){
 element = document.getElementById("titulo");
 typeWriter(element);
 
+// Obtener todas las etiquetas <a> con href que comienzan con '#'
+const enlaces = document.querySelectorAll('a[href^="#"]');
+
+// Escuchar el evento click en cada enlace
+enlaces.forEach(enlace => {
+  enlace.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+
+    const seccionId = this.getAttribute('href'); // Obtener el ID de la sección a la que apunta el enlace
+    const seccion = document.querySelector(seccionId); // Obtener la sección
+
+    if (seccion) {
+      const navbarHeight = document.getElementById('header').offsetHeight; // Obtener la altura de la navbar
+    
+      const seccionOffsetTop = seccion.offsetTop; // Obtener la posición superior de la sección
+
+      // Ajustar el desplazamiento teniendo en cuenta la altura de la navbar
+      window.scrollTo({
+        top: seccionOffsetTop - navbarHeight,
+        behavior: 'smooth' // Desplazamiento suave
+      });
+    }
+  });
+});
